4574424be8d212470f222cda77fa60706e3a9151
Thendrum <77357730+Thendrum@users.noreply.github.com>
2022-10-19 17:12:45 -0400

    Ball move script
    title

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public Rigidbody2D Puck;
+    public float PushForce;
+    public float X;
+    public float Y;
+    public Vector2 PushVector;
+
+    void Start()
+        {
+            X = -10f;
+            Y = Random.Range(-6f, 6f);
+            PushVector = new Vector2(X, Y);
+            Puck.AddForce(PushVector * PushForce);
+
+
+        //
+        }
+    }
